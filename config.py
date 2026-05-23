47133bd55e4964cd934f447996754cbe9a2a9ab9
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-23 19:41:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VAMPIREUPDATES")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VAMPIREUPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BIHARIXPOWER")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BIHARIXPOWER")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
