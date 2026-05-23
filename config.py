8a282186a1cee5561780ec736ad3d01a7f7a2e36
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-23 19:49:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VAMPIRE-BOTS",
+    "https://github.com/VAMPIRE-BOTS/pintu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
